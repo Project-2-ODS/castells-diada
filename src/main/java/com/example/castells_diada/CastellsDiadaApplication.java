0f2d59990ad285d64efcf900a391cc39ddce06af
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-22 15:58:03 +0200

    first commit

---
+++ src/main/java/com/example/castells_diada/CastellsDiadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.castells_diada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastellsDiadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastellsDiadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/castells_diada/CastellsDiadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.castells_diada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastellsDiadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
