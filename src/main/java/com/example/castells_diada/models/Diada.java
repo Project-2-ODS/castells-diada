d5eed521b02ea00cd30a7634cfcd0a1d6f3a370d
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-26 17:53:39 +0200

    model init

---
+++ src/main/java/com/example/castells_diada/models/Diada.java
@@ -0,0 +1,27 @@
+package com.example.castells_diada.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Diada {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private LocalDate diadaDate;
+    private Long capid;
+}
