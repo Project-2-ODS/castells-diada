42c61da60c1a2d641e4a08a83e2dd8fef5236917
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-26 18:05:46 +0200

    repository created

---
+++ src/main/java/com/example/castells_diada/repositories/DiadaRepository.java
@@ -0,0 +1,7 @@
+package com.example.castells_diada.repositories;
+
+import com.example.castells_diada.models.Diada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiadaRepository extends JpaRepository<Diada, Long> {
+}
